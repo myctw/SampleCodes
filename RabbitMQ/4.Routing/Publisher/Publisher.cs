43d72b1aacd1affab6a7b396b92f77f19d3bd73c
myctw <44391355+myctw@users.noreply.github.com>
2018-11-24 00:01:40 +0800

    Add files via upload

---
+++ RabbitMQ/4.Routing/Publisher/Publisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var msg_error = $"This is Error msg.";
+                var msg_info = $"This is Info msg";
+
+                channel.BasicPublish(exchange: "direct_logs",
+                    routingKey: "ERROR",
+                    basicProperties: null,
+                    body: Encoding.UTF8.GetBytes(msg_error));
+                channel.BasicPublish(exchange: "direct_logs",
+                    routingKey: "INFO",
+                    basicProperties: null,
+                    body: Encoding.UTF8.GetBytes(msg_info));
+
+
+            }
+        }
+    }
+}
---
+++ RabbitMQ/4.Routing/Subscriber/Subscriber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Subscriber
+{
+    class Subscriber
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: "INFO");
+                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: "WARNING");
+                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: "ERROR");
+                channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: "FATAL");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var msg = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine($"Receiving RoutingKey:{routingKey}, Msg:{msg}.");
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press any to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
